74dd52ee40b7d8a61c6eb45af6c14ba9eb62f95c
Daniel Eloy <dans_eloy@hotmail.com>
2025-07-26 12:59:01 -0300

    Projeto TypeScript + React Finalizado

--- Desafio05/RID183995_Desafio05/src/App.tsx
+++ Desafio05/RID183995_Desafio05/src/App.tsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { useTheme } from './ThemeContext.tsx'
 import './App.css'
 
+interface TodoItem {
+  id: string,
+  texto: string,
+  completado: boolean
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const chaveTarefasMemoria = "tarefas"
+
+  const {theme, toggleTheme} = useTheme()
+  const [todos, setTodos] = useState<TodoItem[]>([])
+  const [novoTodo, setnovoTodo] = useState<string>("")
+  const [estaCarregado, setEstaCarregando] = useState<boolean>(false)
+
+  const adicionarTarefa = (): void => {
+    if (novoTodo !== "") {
+      const newId = crypto.randomUUID()
+      const novoTodoItem: TodoItem = {
+        id: newId,
+        texto: novoTodo,
+        completado: false
+      }
+      setTodos([...todos, novoTodoItem])
+      setnovoTodo("")
+    }
+  }
+
+  const removerTarefa = (id: string): void => {
+    const todosAtualizados = todos.filter((todo) => todo.id !== id)
+    setTodos(todosAtualizados)
+  }
+
+  const marcarCompleto = (id: string): void => {
+    const todosAtualizados = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completado: !todo.completado }
+      }
+      return todo
+    })
+    setTodos(todosAtualizados)
+  }
+
+  const obterTarefasCompletas = (): TodoItem[] => {
+    return todos.filter((todo) => todo.completado)
+  }
+
+  useEffect(() => {
+    if (estaCarregado) {
+      setEstaCarregando(true)
+      localStorage.setItem(chaveTarefasMemoria, JSON.stringify(todos))
+    }
+  }, [todos, estaCarregado])
+
+
+  useEffect(() => {
+    const tarefasDaMemoria = localStorage.getItem(chaveTarefasMemoria)
+    if (tarefasDaMemoria) {
+      setTodos(JSON.parse(tarefasDaMemoria))
+    }
+    setEstaCarregando(true)
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TypeScript + React + DNC</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className= {`app ${theme}`}>
+      <div className={`container ${theme}`}>
+        <h1>Lista de Tarefas - {obterTarefasCompletas().length} / {todos.length}</h1>
+        <div className='input-container'>
+          <input type="text" value={novoTodo} onChange={(e) => setnovoTodo(e.target.value)} />
+          <button onClick={adicionarTarefa}>Adicionar Tarefa</button>
+        </div>
+        <ol>
+          {
+            todos.map((todo) => (
+              <li key={todo.id}>
+                <input type="checkbox" checked={todo.completado} onChange={() => marcarCompleto(todo.id)} />
+                <span style={{ textDecoration: todo.completado ? 'line-through' : 'none'}}> {todo.texto} </span>
+                <button onClick={() => removerTarefa(todo.id)}>Remover</button>
+              </li>
+            ))
+          }
+        </ol>
+        <button onClick={toggleTheme}>
+          Alterar para o tema {theme === 'light' ? 'Escuro' : 'Claro'}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ Desafio05/RID183995_Desafio05/src/ThemeContext.tsx
@@ -0,0 +1,33 @@
+import type { ReactNode } from "react"
+import { createContext, useContext, useState } from "react";
+
+interface ThemeContextType {
+    theme: 'light' | 'dark'
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export const ThemeProvider = ({ children }: { children: ReactNode}) => {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light')
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+
+}
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error("useTheme deve ser utilizado como um ThemeProvider")
+    }
+    return context
+}
